5a727b131c7b9aec133a8fecb3b0781fd768f4f7
pashasport <123305807+pashasport@users.noreply.github.com>
2023-08-25 16:24:05 +0300

    update-react-hw-feedback-02

---
+++ src/components/App/App.styled.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  padding: 50px;
+
+  display: flex;
+  flex-direction: column;
+`;
---
+++ src/components/Statistics/Statistics.styled.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const List = styled.ul`
+  list-style: none;
+  display: flex;
+  gap: 10px;
+  flex-direction: column;
+  text-aling: center;
+  font-size: 22px;
+`;
